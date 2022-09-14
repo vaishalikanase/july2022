baf26f6d4ced48774a8c98c8fd82971aab51d863
sangr <sangr@LAPTOP-2DQJ1VS0>
2022-09-14 23:15:54 +0530

    hello vaishali

---
+++ src/sunday/com/java/SampleDemo.java
@@ -0,0 +1,10 @@
+package sunday.com.java;
+
+public class SampleDemo {
+public static void main(String[] args)
+{
+	String s="vaishali";
+	String a=s.substring(3,5);
+	System.out.println(a);
+}
+}
---
+++ src/sunday/com/java/splistringsemo.java
@@ -0,0 +1,12 @@
+package sunday.com.java;
+
+public class splistringsemo 
+{
+public static void main(String[] args) {
+	String a="vai123shali89";
+	 String b=a.substring(3,6);
+	 System.out.println(b);
+	
+}
+
+}
